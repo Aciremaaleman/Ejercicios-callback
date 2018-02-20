b133f8fc4a807730bbeb988f0854f73cf282376b
aciremaaleman <aciremaaleman@outlook.es>
2018-02-20 10:23:00 -0600

    Actualizando funcion tipo sort

--- app.js
+++ app.js
@@ -26,62 +26,60 @@ function miMap(arreglo, callback){
 
 
 // 3 ) Crea tu propia función filter que funcione igual a Array.filter. Algo asi:
-// var newArray = [];
-// function miFilter(arreglo, filter){
-//     console.log('Ejecicio 3 reemplanzando filter');
-//     console.log('Devuelve todo el arreglo' + ' ' + arreglo);
-//     for (var i = 0; i < arreglo.length; i++){
-//         if(arreglo[i] === 'a'){
-//             newArray.push('a');
-//             console.log('Devuelve el nuevo array con los elementos filtrados' + ' ' + newArray);
-//         };
-//     };
-// };
-// miFilter(['a','b','c','d','a','d','a','w','a','z','a'], function(callback){})
+var newArray = [];
+function miFilter(arreglo, filter){
+    console.log('Ejecicio 3 reemplanzando filter');
+    console.log('Devuelve todo el arreglo' + ' ' + arreglo);
+    for (var i = 0; i < arreglo.length; i++){
+        if(arreglo[i] === 'a'){
+            newArray.push('a');
+            console.log('Devuelve el nuevo array con los elementos filtrados' + ' ' + newArray);
+        };
+    };
+};
+miFilter(['a','b','c','d','a','d','a','w','a','z','a'], function(callback){})
 
 // 4 ) Crea tu propia función reduce que funcione igual a Array.reduce. Algo asi:
-// var count = 0;
-// function miReduce(arreglo, reduce){
-//     console.log('Ejecicio 4 reemplanzando reduce');
-//     console.log('Devuelve todo el arreglo para aplicar miReduce' + ' ' + arreglo);
-//     for (var i = 0; i < arreglo.length; i++){
-//         count = count + i;
-//         console.log('el total es' + count);
-//     };
-// };
-// miReduce([0,1,2,3,4], function(callback){})
+var count = 0;
+function miReduce(arreglo, reduce){
+    console.log('Ejecicio 4 reemplanzando reduce');
+    console.log('Devuelve todo el arreglo para aplicar miReduce' + ' ' + arreglo);
+    for (var i = 0; i < arreglo.length; i++){
+        count = count + i;
+        console.log('el total es' + count);
+    };
+};
+miReduce([0,1,2,3,4], function(callback){})
 
 // 1 ) Crea tu propia función sort que funcione igual a Array.sort. Algo asi
-// function burbuja(arreglo){
-//     console.log('Resultado en pantalla ejercicio 6 reemplazando Sort')
-//         // console.log(arreglo.reverse());
-//         for (var i = 1; i < arreglo.length; i++){
+function burbuja(arreglo){
+    console.log('Resultado en pantalla ejercicio 6 reemplazando Sort')
+        for (var i = 1; i < arreglo.length; i++){
 
-//             for ( var j = 0; j < (arreglo.length-i); j++){
+            for ( var j = 0; j < (arreglo.length-i); j++){
 
-//                 if (arreglo[j] > arreglo[j+1]){
+                if (arreglo[j] > arreglo[j+1]){
 
-//                     k=arreglo[j+1];
-//                     arreglo[j+1] = arreglo[j];
-//                     arreglo[j] = k;
-//                 }
-//             }
-//         }
-//         return(arreglo);
-// }
+                    k=arreglo[j+1];
+                    arreglo[j+1] = arreglo[j];
+                    arreglo[j] = k;
+                }
+            }
+        }
+        return(arreglo);
+}
 
-// arreglo = Array(4,5,3,2,6,1,8,7,9);
+arreglo = Array(4,5,3,2,6,1,8,7,9);
 
-// document.write('Valores iniciales <br>');
-// for (var i = 0; i < arreglo.length; i++){
-//     document.write(arreglo[i] + '<br>');
-// }
+document.write('Valores iniciales <br>');
+for (var i = 0; i < arreglo.length; i++){
+    document.write(arreglo[i] + '<br>');
+}
 
-// arrayB = burbuja(arreglo);
+arrayB = burbuja(arreglo);
 
-// document.write('Valores ordenados <br>');
-// for (var i = 0; i < arrayB.length; i++){
-//     document.write(arrayB[i] + '<br>');
-// }
-// miSort(['watermelon','kiwi','banana','apple'], function(callback){})
-// miSort([4,5,3,2,6,1,8,7,9], function(callback){})
+document.write('Valores ordenados <br>');
+for (var i = 0; i < arrayB.length; i++){
+    document.write(arrayB[i] + '<br>');
+};
+
